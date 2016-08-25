e2316dec98d95afede286af55f6fe527e44102fe
Ed Scheinerman <ers@jhu.edu>
2016-08-25 15:27:10 -0400

    Still very early

--- src/SimpleTropical.jl
+++ src/SimpleTropical.jl
@@ -2,11 +2,15 @@
 
 module SimpleTropical
 
-export Tropical
+import Base.isinf, Base.show, Base.promote_rule
+
+export Tropical, TropicalInf
 
 immutable Tropical{T<:Real} <: Number
   x::T
   inf_flag::Bool
+
+
   function Tropical(xx::Real, ii::Bool=false)
     TT = typeof(xx)
     if isinf(xx) || ii
@@ -16,8 +20,21 @@ immutable Tropical{T<:Real} <: Number
   end
 end
 
-promote_rule{S<:Real, T<:Real}(::Type{Tropical{S}},::Type{Tropical{T}}) =
-  Tropical{promote_type{S,T}}
+Tropical{T<:Real}(x::T) = Tropical{T}(x)
+const TropicalInf = Tropical{Bool}(0,true)
+
+isinf(x::Tropical) = x.inf_flag
+
+promote_rule{S<:Real, T<:Real}(::Type{Tropical{S}},::Type{Tropical{T}}) = Tropical{promote_type(S,T)}
+
+
+function show(io::IO, t::Tropical)
+  if isinf(t)
+    print(io,"Tropical($(Char(8734)))")
+  else
+    print(io,"Tropical{$(typeof(t.x))}($(t.x))")
+  end
+end
 
 
 end # end of module
