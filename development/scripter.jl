197911492c8bda1c59c367b1baf584b98a34e9d5
Ed Scheinerman <ers@jhu.edu>
2024-10-12 21:01:15 -0400

    All integers

--- development/scripter.jl
+++ development/scripter.jl
@@ -1,16 +1,21 @@
 using SimpleTropical
 
-function _sub_digit(n::Int)::Char
+function _sub_digit(n::Integer)::Char
     chars = ['₀', '₁', '₂', '₃', '₄', '₅', '₆', '₇', '₈', '₉']
     return chars[n+1]
 end
 
-function _sup_digit(n::Int)::Char
+function _sup_digit(n::Integer)::Char
     chars = ['⁰', '¹', '²', '³', '⁴', '⁵', '⁶', '⁷', '⁸', '⁹']
     return chars[n+1]
 end
 
-function _make_script(n::Int, sub::Bool)
+"""
+    _make_script(n::Integer, sub::Bool)
+
+Common engine for `int2sub` and `int2sup`.
+"""
+function _make_script(n::Integer, sub::Bool)
     method = sub ? _sub_digit : _sup_digit
     if n >= 0
         digs = reverse(digits(n))
@@ -35,11 +40,11 @@ julia> println("x" *  int2sub(17))
 x₁₇
 ```
 """
-int2sub(n::Int)::String = _make_script(n, true)
+int2sub(n::Integer)::String = _make_script(n, true)
 
 
 """
-    int2sup(n::Int)::String
+    int2sup(n::Integer)::String
 
 Given an integer `n`, return `n` as as a base-10
 string using *superscript* digits. 
@@ -51,14 +56,5 @@ julia> println("x" * int2sup(-2))
 x⁻²
 ```
 """
-int2sup(n::Int)::String = _make_script(n, false)
+int2sup(n::Integer)::String = _make_script(n, false)
 
-
-
-# struct TropicalMonomial
-#     coef::Tropical
-#     exps::Dict{Int,Int}
-
-
-
-# end
